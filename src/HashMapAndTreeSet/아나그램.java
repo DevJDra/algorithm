301c1ec693fd5cf5fcce7a5b6de29ce0bfb1bfa7
DevJDra <jyahn3752@gmail.com>
2023-02-16 22:25:38 +0900

    2023-02-16 아나그램 commit

--- src/HashMapAndTreeSet/아나그램.java
+++ src/HashMapAndTreeSet/아나그램.java
@@ -2,6 +2,8 @@ package HashMapAndTreeSet;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class 아나그램 {
     public static void main(String args[]) throws IOException {
@@ -25,4 +27,27 @@ public class 아나그램 {
         }
         System.out.println(anagram);
     }
+/*
+    public String solution(String s1, String s2) {
+        String answer = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char x : s1.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0)+1);
+        }
+
+        for(char x : s2.toCharArray()) {
+            if(!map.containsKey(x) || map.get(x) == 0) return "NO";
+            map.put(x, map.get(x) - 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        아나그램 T = new 아나그램();
+        Scanner kb = new Scanner(System.in);
+        String a = kb.next();
+        String b = kb.next();
+        System.out.println(T.solution(a, b));
+    }
+ */
 }
