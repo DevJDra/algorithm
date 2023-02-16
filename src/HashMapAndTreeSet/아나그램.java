22a544c5081fba5e8da23053ef14188bfb8006e2
DevJDra <jyahn3752@gmail.com>
2023-02-16 21:43:03 +0900

    2023-02-16 아나그램 commit

--- src/HashMapAndTreeSet/아나그램.java
+++ src/HashMapAndTreeSet/아나그램.java
@@ -7,7 +7,6 @@ public class 아나그램 {
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String a = br.readLine();
         String[] arr = a.split("");
