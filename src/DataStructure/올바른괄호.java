551114df2201a57423660ee3f87050279509852b
DevJDra <jyahn3752@gmail.com>
2023-02-20 21:15:35 +0900

    2023-02-20 올바른괄호 commit

--- src/DataStructure/올바른괄호.java
+++ src/DataStructure/올바른괄호.java
@@ -1,9 +1,38 @@
 package DataStructure;
 
 import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class 올바른괄호 {
 
+    public String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for(char x : str.toCharArray()) {
+            if(x == '(') {
+                stack.push(x);
+            }
+            else {
+                if(stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()) {
+            return "NO";
+        }
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        올바른괄호 T = new 올바른괄호();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+/*
     public String solution(String[] arr) {
 
         int r = 0;
@@ -44,4 +73,5 @@ public class 올바른괄호 {
         bw.write(T.solution(arr));
         bw.close();
     }
+    */
 }
