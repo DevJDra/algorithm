96d10ded1ead063437939cb09d206dc797ba7c7d
DevJDra <jyahn3752@gmail.com>
2023-02-25 22:24:20 +0900

    2023-02-25 공주구하기 commit

---
+++ src/DataStructure/공주구하기.java
@@ -0,0 +1,49 @@
+package DataStructure;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 공주구하기 {
+
+    public String solution(int n, int k) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=1; i<=n; i++) {
+            q.offer(i);
+        }
+
+        while(!q.isEmpty()) {
+            for(int i=1; i<k; i++) {
+                q.offer(q.poll());
+            }
+
+            q.poll();
+            if(q.size() == 1) {
+                answer = q.poll();
+            }
+        }
+
+        return "";
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        공주구하기 T = new 공주구하기();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        bw.write(T.solution(n, k));
+        bw.close();
+    }
+}
